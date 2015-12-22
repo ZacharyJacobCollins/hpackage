db29dadcc81b7556a816b8ae88afce6533004f3b
Zac Collins <zacharyjacobcollins@gmail.com>
2015-12-22 17:30:39 -0500

    updates

---
+++ examples/Concurrency/concurrent-print.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+  "time"
+  "math/rand"
+)
+
+func f(n int) {
+  for i := 0; i < 10; i++ {
+    fmt.Println(n, ":", i)
+    amt := time.Duration(rand.Intn(250))
+    time.Sleep(time.Millisecond * amt)
+  }
+}
+
+func main() {
+  for i := 0; i < 10; i++ {
+    f(i)  //run with and without a goroutine here to understand
+  }
+  var input string
+  fmt.Scanln(&input)
+}
