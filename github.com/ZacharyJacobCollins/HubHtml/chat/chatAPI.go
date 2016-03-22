3eb73180cda2cf0c5733d38366391b8ee0ea7d7e
Zac Collins <zacharyjacobcollins@gmail.com>
2016-03-22 02:24:30 -0400

    up

--- github.com/ZacharyJacobCollins/HubHtml/chat/chatAPI.go
+++ github.com/ZacharyJacobCollins/HubHtml/chat/chatAPI.go
@@ -7,10 +7,13 @@ import (
 	"strings"
 )
 
+type User struct {
+	name string
+	password string
+}
+
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()  //Parse url parameters passed, then parse the response packet for the POST body (request body)
-	// attention: If you do not call ParseForm method, the following data can not be obtained form
-	fmt.Println(r.Form) // print information on server side.
 	fmt.Println("path", r.URL.Path)
 	fmt.Println("scheme", r.URL.Scheme)
 	fmt.Println(r.Form["url_long"])
@@ -24,7 +27,7 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //get request method
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("login.gtpl")
+		t, _ := template.ParseFiles("login.html")
 		t.Execute(w, nil)
 	} else {
 		r.ParseForm()
--- github.com/ZacharyJacobCollins/HubHtml/html/main.go
+++ github.com/ZacharyJacobCollins/HubHtml/html/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
   // w := wiki.NewWiki();  w.Run();
   c := chat.NewChat();  c.Run(3);
-  http.Handle("/html/", http.StripPrefix("/html/", http.FileServer(http.Dir("html"))))
   http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./html/"))))
-	http.ListenAndServe(":1337", nil)
+  http.HandleFunc("/login", login)
+  http.ListenAndServe(":1337", nil)
 }
