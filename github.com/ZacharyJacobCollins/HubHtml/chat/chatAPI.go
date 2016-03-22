55e43b577adedefaf79b4d0c9ce45d8ca2ca522a
Zac Collins <zacharyjacobcollins@gmail.com>
2016-03-22 02:33:41 -0400

    up

--- github.com/ZacharyJacobCollins/HubHtml/chat/chatAPI.go
+++ github.com/ZacharyJacobCollins/HubHtml/chat/chatAPI.go
@@ -4,24 +4,13 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"strings"
+	"log"
 )
 
 type User struct {
-	name string
+	name 	 string
 	password string
-}
-
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()  //Parse url parameters passed, then parse the response packet for the POST body (request body)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello astaxie!") // write data to response
+	image    string
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -31,8 +20,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		t.Execute(w, nil)
 	} else {
 		r.ParseForm()
-		// logic part of log in
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
+		u := User{name: r.Form["username"][0], password: r.Form["password"][0], image:r.Form["image"][0]}
+		log.Print(u.name)
 	}
 }
