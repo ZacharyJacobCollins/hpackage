f54aff412f962d4f09d4d7efe75e9a1770f70f05
Zac Collins <zacharyjacobcollins@gmail.com>
2016-03-22 03:11:12 -0400

    up

--- github.com/ZacharyJacobCollins/HubHtml/chat/chatAPI.go
+++ github.com/ZacharyJacobCollins/HubHtml/chat/chatAPI.go
@@ -1,26 +1,26 @@
 package chat
-
-import (
-	"fmt"
-	"html/template"
-	"net/http"
-	"log"
-)
-
-type User struct {
-	name 	 string
-	password string
-	image    string
-}
-
-func Login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //get request method
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("html/login.html")
-		t.Execute(w, nil)
-	} else {
-		r.ParseForm()
-		u := User{name: r.Form["username"][0], password: r.Form["password"][0], image:r.Form["image"][0]}
-		log.Print(u.name)
-	}
-}
+//
+//import (
+//	"fmt"
+//	"html/template"
+//	"net/http"
+//	"log"
+//)
+//
+//type User struct {
+//	name 	 string
+//	password string
+//	image    string
+//}
+//
+//func Login(w http.ResponseWriter, r *http.Request) {
+//	fmt.Println("method:", r.Method) //get request method
+//	if r.Method == "GET" {
+//		t, _ := template.ParseFiles("html/login.html")
+//		t.Execute(w, nil)
+//	} else {
+//		r.ParseForm()
+//		u := User{name: r.Form["username"][0], password: r.Form["password"][0], image:r.Form["image"][0]}
+//		log.Print(u.name)
+//	}
+//}
--- github.com/ZacharyJacobCollins/HubHtml/html/main.go
+++ github.com/ZacharyJacobCollins/HubHtml/html/main.go
@@ -12,6 +12,6 @@ func main() {
   // w := wiki.NewWiki();  w.Run();
   c := chat.NewChat();  c.Run(3);
   http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./html/"))))
-  http.HandleFunc("/login", chat.Login)
+  //http.HandleFunc("/login", chat.Login)
   http.ListenAndServe(":1337", nil)
 }
